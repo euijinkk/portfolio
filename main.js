9761326814fd449d69b1549012c494dc61f0304e
gytks4 <gytks4@naver.com>
2021-02-21 11:05:39 +0900

    JS: navbar index click to scroll

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ document.addEventListener('scroll',() => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarIndex = document.querySelector('.index ul')
+navbarIndex.addEventListener('click', (event) => {
+    // console.log(event.target);
+
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link==null) {
+        // button말고 빈곳을 클릭했을때, undefined이 나온다.
+        // 이때는 함수를 실행시키지 않고, 빠르게 return하는게 효율적이다.
+        return;
+    } else {
+        console.log(link);
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({behavior:'smooth'});
+    }
+})
